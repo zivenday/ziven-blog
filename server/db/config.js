423305d55acd4510b481041dd1b05d7958b4edee
钟伟 <1240500584@qq.com>
2020-01-02 09:01:42 +0800

    koa server modify

---
+++ server/db/config.js
@@ -0,0 +1,17 @@
+// default.js
+// 设置配置文件
+const config = {
+  // 启动端口
+  port: 3000,
+
+  // 数据库配置
+  database: {
+    DATABASE: "mysql",
+    USERNAME: "root",
+    PASSWORD: "12345678",
+    PORT: "3306",
+    HOST: "65.49.196.42"
+  }
+};
+
+module.exports = config;
---
+++ utils/db/config.js
@@ -0,0 +1,17 @@
+// default.js
+// 设置配置文件
+const config = {
+  // 启动端口
+  port: 26637,
+
+  // 数据库配置
+  database: {
+    DATABASE: "mysql",
+    USERNAME: "root",
+    PASSWORD: "1234",
+    PORT: "3306",
+    HOST: "65.49.196.42"
+  }
+};
+
+module.exports = config;
